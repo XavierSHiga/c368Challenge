fa13451c911a82b9c799261d3731f4608c15da26
XavierSHiga <xavierhiga@gmail.com>
2023-09-18 19:19:54 +0000

    Working python check for compatibility with black formatting. Output written to external file.

--- testCheck.py
+++ testCheck.py
@@ -1,7 +1,21 @@
 import os
 import subprocess
 files = os.listdir()
+testOutput = []
+filesChecked = []
 for file in files:
     if file.endswith('.py'):
-        print(subprocess.check_output(['black', '--check', file]))
-
+        filesChecked.append(file)
+        testOutput.append(subprocess.call(['black', '--check', file], stdout=subprocess.DEVNULL))
+output = "Black formatting compatibility check:\n"
+output += " File Name               Status\n"
+output += "---------------------------------------------------------------------------\n"
+for i in range(0, len(filesChecked)):
+    output += filesChecked[i] + ":              "
+    if testOutput[i] == 0:
+        output += "Passed\n"
+    else:
+        output += "Failed\n"
+f = open('blackFormatCheck.txt', 'w')
+f.write(output)
+f.close()
