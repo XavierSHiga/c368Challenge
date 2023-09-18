9c9cb00caa6811b366a13d09681cd647e34c5953
XavierSHiga <xavierhiga@gmail.com>
2023-09-18 20:22:47 +0000

    Correcting the testFail.py file.

--- testFail.py
+++ testFail.py
@@ -2,7 +2,7 @@
 def testFunc():
     a = 1
     b = 2
-    if a == 1   or  b == 3:
-        print('Hello World!')
+    if a == 1 or b == 3:
+        print("Hello World!")
     else:
-        print('Oops')
+        print("Oops")
